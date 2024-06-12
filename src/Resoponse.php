34a8aa0e763ff29c75136e2dbe81daa2aec5d899
Sjouwke <sjouwkevanparys@hotmail.com>
2024-06-12 21:30:58 +0200

    feat: add response class

--- src/Resoponse.php
+++ src/Resoponse.php
@@ -4,4 +4,33 @@ namespace Run\src;
 
 class Response
 {
+    protected $httpStatus = 200;
+    protected $contents = '';
+    protected $header = [];
+
+    public function setHttpStatus(int $statusCode)
+    {
+        $this->httpStatus = $statusCode;
+    }
+
+    public function setContents(string $content)
+    {
+        $this->contents = $content;
+    }
+
+    public function setHeader(string $name, string $value)
+    {
+        $this->header[$name] = $value;
+    }
+
+    public function emit()
+    {
+        http_response_code($this->httpStatus);
+
+        foreach($this->header as $name => $value) {
+            header("$name: $value");
+        }
+
+        echo $this->contents;
+    }
 }
