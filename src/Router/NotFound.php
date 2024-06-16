c0b95c670649bb31f1a95aef6c51c9bc22c65583
Sjouwke <sjouwkevanparys@hotmail.com>
2024-06-17 00:49:33 +0200

    fix(notfound): typo

--- src/Router/NotFound.php
+++ src/Router/NotFound.php
@@ -5,11 +5,11 @@ namespace Run\Router;
 use Exception;
 
 /**
- * Extend from build in PHP Exception class.
+ * Extend from built in PHP Exception class.
  * Info: https://www.php.net/manual/en/language.exceptions.extending.php
  */
 class NotFound extends Exception
 {
-    protected $code = 404;
     protected $message = 'Route not found';
+    protected $code = 404;
 }
