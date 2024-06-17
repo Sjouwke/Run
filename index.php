38378bdc0d176c411d6b7919c5b326795284d210
Sjouwke <sjouwkevanparys@hotmail.com>
2024-06-17 08:32:37 +0200

    feat: throw exception from controller

--- index.php
+++ index.php
@@ -11,16 +11,23 @@ use Run\Router\Router;
 use Run\Controllers\HomeController;
 use Run\Controllers\BlogController;
 use Run\Controllers\ProductController;
+use Run\Router\NotFound;
 
 $router = new Router();
 $router->get('/', [HomeController::class, 'index']);
 $router->get('blog/', [BlogController::class, 'index']);
 $router->get('producten/', [ProductController::class, 'index']);
 $router->get('producten/bekijk/', function() {
+    $product = null;
+
+    if (!$product) {
+        throw new NotFound();
+    }
+
     return 'Dit is een product view!';
 });
 
 // The route() method SHOULD print the output of the registered function for the given path
-// $router->route('producten/bekijk/');
+$router->route('producten/bekijk/');
 // $router->route('producten/');
-$router->route('ikebstanie/');
+// $router->route('ikbestanie/');
